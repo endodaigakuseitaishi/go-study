f3c6546dd26ba206b68039614803761cf3a55eba
遠藤大志 <endoutaishi@endouhiroshinoMacBook-Pro.local>
2023-05-03 00:17:07 +0900

    関数について

--- main.go
+++ main.go
@@ -1,22 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// func plus(x int, y int) int {
+// 	return x + y
+// }
+
+// func div(x, y int) (int, int) {
+// 	q := x / y
+// 	r := x % y
+// 	return q, r
+// }
+
+// func double(price int) (result int) {
+// 	// resultは返り値
+// 	result = price * 2
+//   return
+// }
+
+// 関数を返す関数
+// func returnFunc() func()  {
+// 	return func() {
+// 		fmt.Println("return func()")
+// 	}
+// }
+
+// クロージャー
+// func later() func(string) string {
+// 	var store string
+// 	return func(next string) string {
+// 		s := store
+// 		store = next
+// 		return s
+// 	}
+// }
+
+// ジェネレーター
+func integers() func() int {
+	i := 0
+	return func() int {
+    i++
+    return i
+  }
+}
 
 func main() {
-	// 算術演算子
-	fmt.Println(1 + 3)
-	fmt.Println(1 - 3)
-	fmt.Println(1 * 3)
+	// i := plus(1, 2)
+	// fmt.Println(i)
 
-	s := "ABC"
-	s += "DEF"
-	fmt.Println(s)
+	// i2, i3 := div(1, 2)
+	// fmt.Println(i2, i3)
 
-	// 比較演算子
-	fmt.Println(1 == 1)
-	fmt.Println(1!= 1)
-	fmt.Println(3 > 1)
+	// i4 := double(100)
+	// fmt.Println(i4)
 
-	// 論理演算子
-	fmt.Println(true || true == false)
+	// 無名関数
+	// f := func(x, y int) int {
+	// 	return x + y
+	// }
+	// i := f(1, 2)
+	// fmt.Println(i)
+
+	// f := returnFunc()
+	// f()
+
+	// f := later()
+	// fmt.Println(f("a"))
+
+	ints := integers()
+	fmt.Println(ints())
+	fmt.Println(ints())
+	fmt.Println(ints())
+	fmt.Println(ints())
+	fmt.Println(ints())
 }
