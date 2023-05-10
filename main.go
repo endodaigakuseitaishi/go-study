b69735249110b2966ee5fc856790de023de561af
遠藤大志 <endoutaishi@endouhiroshinoMacBook-Pro.local>
2023-05-10 23:46:54 +0900

    pointer

--- main.go
+++ main.go
@@ -7,157 +7,41 @@ import (
 )
 
 func main() {
-	// channel
-	// var ch1 chan int 
+	var n int = 100
+	// fmt.Println(n)
 
-	// 受信専用
-	// var ch2 <-chan int
+	// fmt.Println(&n)
 
-	// 送信専用
-	// var ch2 chan<- int
+	// Double(n)
+	// fmt.Println(n)
 
-	// ch1 = make(chan int)
-	// ch2 := make(chan int)
+	var p *int = &n
+	// fmt.Println(p)
+	// fmt.Println(*p)
 
-	// fmt.Println(cap(ch1))
-	// fmt.Println(cap(ch2))
+	// *p = 300
+	// n = 200
+  Doublev2(&n)
+	fmt.Println(n)
+	fmt.Println(*p)
 
-	// ch3 := make(chan int, 5)
-	// fmt.Println(cap(ch3))
+	var sl []int = []int{1, 2, 3, 4, 5, 6}
+	Doublev3(sl)
+	fmt.Println(sl)
+}
 
-	// ch3に送信
-	// ch3 <- 1
-	// fmt.Println(len(ch3))
-
-	// iに送信
-	// i := <-ch3
-	// fmt.Println(i)
-	// i2 := <-ch3
-	// fmt.Println(i2)
-
-	// ch3 <- 1
-	// fmt.Println("========")
-	// fmt.Println(len(ch3))
-	// fmt.Println(<-ch3)
-	// ch3 <- 2
-	// ch3 <- 3
-	// ch3 <- 4
-	// ch3 <- 5
-	// ch3 <- 6
-
-	// ch4 := make(chan int)
-	// ch5 := make(chan int)
-
-	// go reciever(ch4)
-	// go reciever(ch5)
-
-	// i := 0
-
-	// for i < 100 {
-	// 	ch4 <- i
-	// 	ch5 <- i
-	// 	time.Sleep(50*time.Millisecond)
-	// 	i++
-	// }
-
-	// ch6 := make(chan int, 2)
-	// go reciever("1.",ch6)
-	// go reciever("2.",ch6)
-	// go reciever("3.",ch6)
-
-	// i := 0
-	// for i < 100 {
-  //   ch6 <- i
-  //   i++
-  // }
-	// close(ch6)
-	// time.Sleep(3*time.Second)
-
-	// chが空かつchがcloseでfalseを返す
-	// i, ok := <-ch6
-	// fmt.Println(i, ok)
-
-	// ch7 := make(chan int, 3)
-	// ch7 <- 1
-	// ch7 <- 2
-	// ch7 <- 3
-  // close(ch7)
-
-	// for i := range ch7 {
-	// 	fmt.Println(i)
-	// }
-
-	// ch8 := make(chan int, 2)
-	// ch9 := make(chan string, 2)
-
-	// ch9 <- "a"
-	// ch9 <- "b"
-	// ch8 <- 2000
-	// ch8 <- 1000
-	// v1 := <-ch8
-	// v2 := <-ch9
-	// fmt.Println(v1)
-	// fmt.Println(v2)
-
-	// selectの中はランダム選択
-	// select {
-	// case v1 := <-ch8:
-	// 	fmt.Println(v1 + 1000)
-	// case v2 := <-ch9:
-	// 	fmt.Println(v2 + "AAA")
-	// default:
-	// 	fmt.Println("どちらでも")
-	// }
-
-	ch10 := make(chan int)
-	ch11 := make(chan int)
-	ch12 := make(chan int)
-
-	// reciever
-	go func() {
-		for{
-			i := <-ch10
-			ch11 <- i*2
-		}
-	}()
-
-	go func() {
-		for{
-			i2 := <-ch11
-			ch12 <- i2-2
-		}
-	}()
-	
-	n := 0
-	for {
-		select {
-			case ch10 <- n:
-				n++
-			case i3 := <-ch12:
-				fmt.Println("received!!!", i3)
-		}
-		if n >= 100 {
-      break
-    }
+func Doublev3(s []int) {
+  for i, v := range s {
+		s[i] = v * 2
 	}
 }
 
-// func reciever(c chan int) {
-// 	for {
-// 		i := <-c
-// 		fmt.Println(i + 3)
-// 	}
-// }
+func Double(i int) {
+	i = i*2
+}
 
-// func reciever(name string, c chan int) {
-// 	for {
-// 		i, ok := <-c
-// 		if !ok {
-// 			break
-// 		}
-// 		fmt.Println(name, i)
-// 	}
-// 	fmt.Println(name, "done")
-// }
+func Doublev2(i *int) {
+	*i = *i * 2
+}
 
 
