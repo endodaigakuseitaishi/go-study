f209a6e7f53527a75573ba74fb26d998c3084c6c
遠藤大志 <endoutaishi@endouhiroshinoMacBook-Pro.local>
2023-05-11 01:57:06 +0900

    struct method

--- main.go
+++ main.go
@@ -12,49 +12,22 @@ type User struct {
 	// x, y float64
 }
 
+func (u User) SayName() {
+	fmt.Println(u.Name)
+}
+
+func (u *User) SetName(name string) {
+	u.Name = name
+}
+
 func main() {
-	var user1 User
-	fmt.Println(user1)
-	user1.Name = "John Doe"
-	user1.Age = 10
-	fmt.Println(user1)
+	user1 := User{Name: "test"}
+	user1.SayName()
 
-	user2 := User{}
-	fmt.Println(user2)
+	user1.SetName("aaaaaaaaaaaaaaaaaaaa")
+	user1.SayName()
 
-	user3 := User{Name: "aaa", Age: 12}
-	fmt.Println(user3)
-
-	// 構造体の順番に従う必要あり
-	// User{12, "test"}はNG
-	user4 := User{"bbb", 13}
-	fmt.Println(user4)
-
-	user5 := User{Name: "ccc"}
-	fmt.Println(user5)
-
-	// 構造体のポインタ型を返す
-	user6 := new(User)
-	fmt.Println(user6)
-
-	user7 := &User{}
-	fmt.Println(user7)
-
-	UpdateUser(user1)
-	UpdateUser2(user7)
-
-	fmt.Println(user1)
-	fmt.Println(user7)
-}
-
-// 構造体
-func UpdateUser(user User) {
-	user.Name = "test"
-	user.Age = 12
-}
-
-// 参照渡し
-func UpdateUser2(user *User) {
-	user.Name = "test"
-	user.Age = 12
+	user2 := &User{Name: "test333"}
+	user2.SetName("aaaaaaaaaaaa")
+	user2.SayName()
 }
